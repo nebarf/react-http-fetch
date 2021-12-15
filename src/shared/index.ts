381908721fddb6aee32528fd4b9d6f0afd561252
nebarf <francesco.benedetto@sistinf.it>
2021-12-15 10:54:24 +0100

    Revert exposed api

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,6 @@
 export * from './use-compare-callback';
 export * from './use-compare-effect';
+export * from './use-compare-layout-effect';
 export * from './use-compare-memo';
 export * from './use-compare-ref';
 export * from './types';
