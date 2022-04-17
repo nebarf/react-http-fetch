9f5d3fd5d0829ce781383ccdf1998e29d73f9eea
skybenedettof <francesco.benedetto@skytv.it>
2022-04-17 20:10:00 +0200

    Use previous test setup against React v17 test suite only

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,11 @@
 import { enableFetchMocks } from 'jest-fetch-mock';
+import { renderHook } from '@testing-library/react-hooks';
+
+if (process.env.REACT_VERSION === 'v17') {
+  jest.mock('@testing-library/react', () => ({
+    ...(jest.requireActual('@testing-library/react') as Record<string, unknown>),
+    renderHook,
+  }));
+}
 
 enableFetchMocks();
